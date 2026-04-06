9581bb48e1fa6d31efb2b4d3bc900a9b14596fe7
unknown <sgkhenat94@gmail.com>
2026-04-06 18:03:40 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tracelens.echodrop',
+  appName: 'EchoDrop',
+  webDir: 'www'
+};
+
+export default config;
